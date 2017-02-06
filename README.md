db56ef168212a3a7c39a11863a23ff95827b0f85
Denis Ricci <denis.riccis@gmail.com>
2017-02-06 21:27:11 -0200

    Aula 1

---
+++ README.md
@@ -0,0 +1,7 @@
+# alura-spring-mvc
+Projeto criado com Jboss Forge<br>
+https://forge.jboss.org/download
+
+1- ./forge
+2- project-new --named alura-spring-mvc
+
---
+++ src/main/java/br/com/alura/loja/config/ServletSpringMvc.java
@@ -0,0 +1,24 @@
+package br.com.alura.loja.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class ServletSpringMvc extends AbstractAnnotationConfigDispatcherServletInitializer{
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		// TODO Auto-generated method stub
+		return new Class[]{AppWebConfiguration.class};
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[]{"/"};
+	}
+
+}
