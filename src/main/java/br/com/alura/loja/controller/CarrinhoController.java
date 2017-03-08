765e1725fa7808ed53c43a4de8ee0567ef8fa905
Denis Ricci <denis.riccis@gmail.com>
2017-03-08 20:22:54 -0300

    Aula 12 - Escopo de sessao

--- src/main/java/br/com/alura/loja/controller/CarrinhoController.java
+++ src/main/java/br/com/alura/loja/controller/CarrinhoController.java
@@ -1,10 +1,11 @@
 package br.com.alura.loja.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.servlet.ModelAndView;
 
 import br.com.alura.loja.dao.ProdutoDAO;
@@ -15,6 +16,7 @@ import br.com.alura.loja.model.Produto;
 
 @Controller
 @RequestMapping("/carrinho")
+@Scope(value=WebApplicationContext.SCOPE_REQUEST)
 public class CarrinhoController {
 
 	@Autowired
--- src/main/java/br/com/alura/loja/model/CarrinhoCompras.java
+++ src/main/java/br/com/alura/loja/model/CarrinhoCompras.java
@@ -3,9 +3,12 @@ package br.com.alura.loja.model;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.WebApplicationContext;
 
 @Component
+@Scope(value=WebApplicationContext.SCOPE_SESSION)
 public class CarrinhoCompras {
 
 	private Map<CarrinhoItem, Integer> itens = new LinkedHashMap<>();
